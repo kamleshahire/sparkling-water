310cf80623f576f1f5e54fd9c16a7e7b3a7c629f
Navdeep Gill <navdeep@h2o.ai>
2016-10-17 18:49:27 -0700

    [RSparkling] Specify version of SW

--- r/rsparkling/R/package.R
+++ r/rsparkling/R/package.R
@@ -1,13 +1,15 @@
+#Set Sparkling Water version. Default is 1.6.7
+sw_version <- getOption("rsparkling.sparklingwater.version", default = "1.6.7")
 
 #' @import sparklyr h2o utils
 NULL
 
 # define required spark packages
-spark_dependencies <- function(spark_version, scala_version, ...) {
+spark_dependencies <- function(spark_version, scala_version,sw_version, ...) {
   sparklyr::spark_dependency(packages = c(
-    sprintf("ai.h2o:sparkling-water-core_%s:1.6.7", scala_version),
-    sprintf("ai.h2o:sparkling-water-ml_%s:1.6.7", scala_version),
-    sprintf("ai.h2o:sparkling-water-repl_%s:1.6.7", scala_version)
+    sprintf("ai.h2o:sparkling-water-core_%s:%s", scala_version,sw_version),
+    sprintf("ai.h2o:sparkling-water-ml_%s:%s", scala_version,sw_version),
+    sprintf("ai.h2o:sparkling-water-repl_%s:%s", scala_version,sw_version)
   ))
 }
 
